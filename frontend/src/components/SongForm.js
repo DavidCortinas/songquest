637c473c036dff208b33573a1307883fd6927f09
david <dcortinas33@gmail.com>
2023-06-19 00:35:36 -0500

    responsive sidebar and form

--- frontend/src/components/SongForm.js
+++ frontend/src/components/SongForm.js
@@ -12,25 +12,41 @@ import {
   Grid,
   Snackbar,
   Typography,
+  useMediaQuery,
 } from '@mui/material';
 import { useForm } from 'react-hook-form';
-import { makeStyles } from '@mui/styles';
+import { makeStyles, useTheme } from '@mui/styles';
 import { searchSongRequest } from '../thunks';
 import { clearSearchSongError, searchSongSuccess } from '../actions';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => (
+  {
   card: {
     width: '50rem',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+    },
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
   },
   textField: {
-    width: '25rem',
-    shrink: true,
+    width: '40%',
+    [theme.breakpoints.up('sm')]: {
+      width: '25rem',
+    },
   },
   subHeader: {
-    width: '25rem',
+    width: '40%',
+    [theme.breakpoints.up('sm')]: {
+      width: '25rem',
+    },
   },
   description: {
-    maxWidth: '65rem',
+    maxWidth: theme.breakpoints.up('xl') ? '65rem' : '50rem',
     color: '#6f6f71',
     paddingTop: '1rem',
   },
@@ -42,6 +58,18 @@ const useStyles = makeStyles(() => ({
 }));
 
 export const SongForm = ({ error, onSearchPressed, onDataLoaded }) => {
+  const theme = useTheme();
+  const isXsScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const isSmScreen = useMediaQuery(theme.breakpoints.between('sm', 'md'));
+  const isMdScreen = useMediaQuery(theme.breakpoints.between('md', 'lg'));
+  const isLgScreen = useMediaQuery(theme.breakpoints.between('lg', 'xl'));
+  const isXlScreen = useMediaQuery(theme.breakpoints.up('xl'));
+  console.log('isXsScreen: ', isXsScreen)
+  console.log('isSmScreen: ', isSmScreen)
+  console.log('isMdScreen: ', isMdScreen)
+  console.log('isLgScreen: ', isLgScreen)
+  console.log('isXlScreen: ', isXlScreen)
+
   const { handleSubmit } = useForm();
   const [songValue, setSongValue] = useState('');
   const [performerValue, setPerformerValue] = useState('');
@@ -115,24 +143,37 @@ export const SongForm = ({ error, onSearchPressed, onDataLoaded }) => {
   ) : (
     <>
       <Box className={classes.descriptionBox}>
-        <Typography className={classes.description} variant="h6">
-          Interested in licensing a song but don't know where to start? Let
-          SongQuest find the rights holders for you so you can focus on making
-          the deal. Search for the song that you want to license below.
+        <Typography className={classes.description} variant={isXlScreen ? "h6" : "subtitle1"}>
+          {isXlScreen || isLgScreen || isMdScreen
+          ? "Interested in licensing a song but don't know where to start? Let SongQuest find the rights holders for you so you can focus on making the deal. Search for the song that you want to license below."
+          : "Search for the song that you want to license below"}
         </Typography>
       </Box>
       <Grid container justifyContent="center">
         <Card className={classes.card}>
-          <form onSubmit={handleFormSubmit}>
+          <form className={classes.form} onSubmit={handleFormSubmit}>
             <FormControl>
               <CardHeader
                 title="Song Search"
-                subheader="Please include the name of the artist who performs the song for the quickest and most accurate search result..."
-                subheaderTypographyProps={{ width: '28rem' }}
+                titleTypographyProps={{
+                  width: isSmScreen ? '100%' : '28rem',
+                  variant: isSmScreen || isXsScreen
+                  ? 'h6'
+                  : 'h5'
+                }}
+                subheader={!isXsScreen && "Be sure to include the name of the artist who performs the song for the quickest and most accurate search result"}
+                subheaderTypographyProps={{ 
+                  width: isSmScreen ? '100%' : '28rem', 
+                  variant: isXlScreen || isLgScreen 
+                    ? 'body1'
+                    : 'body2'
+                  }}
               />
               <Box display="flex" justifyContent="center">
                 <TextField
-                  InputLabelProps={{ shrink: true }}
+                  InputLabelProps={{ 
+                    shrink: true, 
+                  }}
                   autoFocus
                   variant="outlined"
                   error={invalidSearch}
@@ -147,7 +188,9 @@ export const SongForm = ({ error, onSearchPressed, onDataLoaded }) => {
               <br />
               <Box display="flex" justifyContent="center">
                 <TextField
-                  InputLabelProps={{ shrink: true }}
+                  InputLabelProps={{ 
+                    shrink: true, 
+                  }}
                   variant="outlined"
                   className={classes.textField}
                   onChange={handlePerformerChange}
