da01c97c1fd62490d7be52006f823e3414fd6192
David Cortinas <dcortinas33@gmail.com>
2024-04-09 13:14:05 -0500

    edit profile

--- frontend/src/components/auth/Onboard.js
+++ frontend/src/components/auth/Onboard.js
@@ -60,16 +60,13 @@ const DisplayNameInput = ({
     };
 
     const onCreateDisplayName = async () => {
-        console.log('create')
         if (!displayNameValue) {
             setInvalidDisplayName(true);
             return;
         };
 
         const savedDisplayName = await onUpdateDisplayName(currentUser?.user.id, displayNameValue);
-        console.log(savedDisplayName)
         if (savedDisplayName) {
-            console.log('if')
             setCurrentStep('birthday');
         };
     };
