ca3268ff7585c672214113f181ccc6ed597ead02
David Cortinas <dcortinas33@gmail.com>
2024-04-22 08:23:13 -0500

    compressed static files

--- frontend/views.py
+++ frontend/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.conf import settings
 import os
 
+# def index(request):
+#     index_html_path = os.path.join(settings.BASE_DIR, 'frontend', 'public', 'index.prod.html')
+#     response = render(request, index_html_path)
+#     response['Content-Security-Policy'] = "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self';"
+#     return response
+
 def index(request):
-    index_html_path = os.path.join(settings.BASE_DIR, 'frontend', 'public', 'index.html')
-    response = render(request, index_html_path)
-    response['Content-Security-Policy'] = "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self';"
-    return response
+    return render(request, 'index.html')
