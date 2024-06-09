d7e35925e5e86914186dcbe6d49857e2b0a1dd9d
DavidCortinas <dcortinas33@gmail.com>
2024-06-09 00:40:29 +0000

    fix static

--- songquest/settings.py
+++ songquest/settings.py
@@ -90,22 +90,16 @@ MIDDLEWARE = [
 ]
 
 # Cache settings
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": "redis://127.0.0.1:6379/1",
-#         "OPTIONS": {
-#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-#         },
-#     }
-# }
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
     }
 }
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
+
 
 # CORS settings
 # CORS_ALLOW_HEADERS = [
